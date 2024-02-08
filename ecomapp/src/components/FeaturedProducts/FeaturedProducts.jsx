0ea1aa1d9b3417a85c3ce80901b1d16cb27d286b
SARTHAK <47379019+iamxarko@users.noreply.github.com>
2024-02-08 07:03:36 +0530

    Adding new component

---
+++ ecomapp/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./FeaturedProducts.scss";
+const FeaturedProducts = () => {
+  return <div className="featuredProducts">FeaturedProducts</div>;
+};
+
+export default FeaturedProducts;
